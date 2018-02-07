e2e671d29357a284b36a2866881a5970b15444c0
yassimortensen <yassimortensen@gmail.com>
2018-02-07 14:13:25 -0500

    added responsive styling to languages component

--- src/components/Languages.js
+++ src/components/Languages.js
@@ -49,30 +49,35 @@ class Languages extends Component {
                 <li>CSS Grid</li>
               </ul>
           </div>
-        <i id="arrowDown" className="fas fa-chevron-up fa-2x" onClick={this.handleExpand}></i>
+        <i id="arrowDown" className="fas fa-chevron-up fa-2x language-icon" onClick={this.handleExpand}></i>
         </div>
       )
     } else {
-      info = <i id="arrowUp" className="fas fa-chevron-down fa-2x" onClick={this.handleExpand}></i>
+      info = <i id="arrowUp" className="fas fa-chevron-down fa-2x language-icon" onClick={this.handleExpand}></i>
     }
 
     return (
       <div style={{backgroundColor: '#f5e3e7', padding:'1%', verticalAlign:'top'}}>
         <div className="language-grid">
           <div className='language'>
-            <p className="padding">backend</p>
-            <i className="far fa-gem fa-3x"></i><br/>
-            <p className="padding">Ruby on Rails</p>
+            <p className="language-text">backend</p>
+            <i className="far fa-gem fa-3x language-icon"></i><br/>
+            <p className="language-text">Ruby on Rails</p>
           </div>
           <div className='language'>
-            <p className="padding">frontend</p>
-            <i className="fab fa-react fa-3x"></i><br/>
-            <p className="padding">Javascript with React and Redux</p>
+            <p className="language-text">frontend</p>
+            <div className="javascript-icons">
+              <div></div>
+              <i className="fab fa-js-square fa-3x language-icon" style={{padding:'1%'}}></i>
+              <i className="fab fa-react fa-3x language-icon" style={{padding:'1%'}}></i><br/>
+              <div></div>
+            </div>
+            <p className="language-text">Javascript with <br/> React and Redux</p>
           </div>
           <div className='language'>
-            <p className="padding">styling</p>
-            <i className="fas fa-code fa-3x"></i><br/>
-            <p className="padding">HTML with Custom CSS</p>
+            <p className="language-text">styling</p>
+            <i className="fas fa-code fa-3x language-icon"></i><br/>
+            <p className="language-text">HTML with <br/> Custom CSS</p>
           </div>
         </div>
         {info}
