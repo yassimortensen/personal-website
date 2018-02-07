544c48b6e9f1b722475b6daae9e7cc9d772d2d94
yassimortensen <yassimortensen@gmail.com>
2018-02-07 15:12:48 -0500

    added responsive styling to personal footer component

--- src/components/Closing.js
+++ src/components/Closing.js
@@ -4,10 +4,10 @@ import '../App.css';
 class Closing extends Component {
   render() {
     return (
-      <div style={{backgroundColor:'#B5DBB4', fontFamily:'Roboto Slab'}}>
-        <p style={{padding: '1%', fontSize:'2em'}}>Want to work together?</p>
-        <p style={{fontSize:'1em'}}>{`I'm currently accepting new projects amd would love to hear about yours.`}</p>
-        <p style={{paddingBottom: '2%', fontSize:'1em'}}>Please take a few minutes to tell me about it</p>
+      <div className="footer">
+        <p className="work-together">Want to work together?</p>
+        <p className="accepting">{`I'm currently accepting new projects and would love to hear about yours.`}</p>
+        <p className="accepting tell-me">Please take a few minutes to tell me about it</p>
       </div>
     );
   }
